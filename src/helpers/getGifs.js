1711eb962bbf4d41a8f7af4b4e6f0a19ce3be811
Ell3dition <marcosjara77@gmail.com>
2021-10-21 17:56:30 -0300

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async (category) => {
+    const url =
+      `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=wuK7MUJ0Z5IDj10CsJWQMf3JnxW6Oaco`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url,
+      };
+    });
+
+    return gifs;
+  };
